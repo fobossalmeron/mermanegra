6e4968729b6a78bedfc60c0f55c9ba3b327fed21
Elkin Urango <kin3xc@hotmail.com>
2018-12-16 14:38:56 -0500

    Changes for prod

--- inc/config.php
+++ inc/config.php
@@ -1,8 +1,3 @@
 <?php
-	// prod
-	// define("BASE_URL","/");
-	// define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/");
-
-	// dev
-	define("BASE_URL","http://localhost/mermanegra/");
-	define("ROOT_PATH",  "/Applications/XAMPP/xamppfiles/htdocs/mermanegra/");
+	define("BASE_URL","/");
+	define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/");
